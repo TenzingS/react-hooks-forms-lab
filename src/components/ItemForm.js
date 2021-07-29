01e212526a9a297a58bc88b5ac1e10b46a6e7a02
TenzingS <thesherpatenzing@gmail.com>
2021-07-28 21:05:42 -0400

    Not passing ItemForm test

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState} from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({ onItemFormSubmit}) {
+  const [formData, setFormData] = useState({
+    category: 'Produce',
+    name: ''
+  })
+
+  function handleFormChange(event) {
+    const name = event.target.name
+    
+    setFormData({
+        ...formData,
+        [name]: event.target.value
+      })
+  
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit = {(event) => onItemFormSubmit(event) }   >
       <label>
         Name:
         <input type="text" name="name" />
@@ -11,7 +26,10 @@ function ItemForm(props) {
 
       <label>
         Category:
-        <select name="category">
+        <select 
+          name="category"
+          onChange={handleFormChange}
+          value= {formData.category}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
